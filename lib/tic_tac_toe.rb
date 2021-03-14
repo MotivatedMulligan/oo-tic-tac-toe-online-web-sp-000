8b851a1ac2104bae189f856af78ee5cd7e7f10c2
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-14 16:28:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,10 +9,10 @@ WIN_COMBINATIONS = [
   [2,5,8],
   [0,4,8],
   [2,4,6]
-]
- def initialize#(board = nil)
+  ]
+  def initialize#(board = nil)
   @board = Array.new(9, " ")#[" "," "," "," "," "," "," "," "," "] #board || Array.new(9, " ")
- end
+  end
 
   def display_board
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -21,6 +21,11 @@ WIN_COMBINATIONS = [
       puts "-----------"
       puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-
-
+  def input_to_index(input)
+  index = input.to_i - 1
+  end
+  def move
+    
+  end
+binding 'pry'
 end
